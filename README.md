e9f57f825382cf753b7ee906d358dddeb52f0bd0
Keshan J <keshanth64@gmail.com>
2019-07-13 00:57:41 +0530

    emojis and json

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ This is a basic chatbot that uses whatsapp web to chat with a given target
 
 However, it can only respond to messages one by one, Multiple messages MIGHT result in inaccurate replies.
 
-
+NOTE : if anything doesn't load or open. Please check:
+1 . The chromedriver path
+2 . The xpath values with your whatsapp web styles
+3 . The target name
+4 . Scan the QR Code within 25 seconds ( can be changed in the init method)
--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -14,9 +14,11 @@ import threading
 import json
 import random
 import re
+import emoji
 
+# Global variables
 learn_count = 0
-no_reply =   [
+no_reply = [
     "sorry, i didnt get that. I'm still learning. ",
     "keshan didnt teach me well. ",
     "excuse me?",
@@ -24,10 +26,15 @@ no_reply =   [
     "english pleasee?"
 ]
 
+
+#  TODO : Handle multiple messages, improve replies accuracy
+# ? Need to find a way to be able to get multiple user inputs
+
+
 class Whatsapp:
     # init method
     def __init__(self):
-        # the path to the chromedriver executable 
+        # the path to the chromedriver executable
         chromepath = r'C:\webdrivers\chromedriver'
         self.driver = webdriver.Chrome(executable_path=chromepath)
         self.driver.get("https://web.whatsapp.com/")
@@ -35,10 +42,6 @@ class Whatsapp:
         # the recipient
         self.target = 'Bro'
         elem = None
-        
-        # open and load the intents json file that has the chat patterns and replies
-        with open('intents.json') as f:
-            self.data = json.load(f)
 
         # search for the target in the contact list
         while elem is None:
@@ -62,25 +65,31 @@ class Whatsapp:
     # method to logout of the web session
     # if needed call function
     def logout(self):
+        # gets the hamburger drop down and clicks it
         ham = self.driver.find_element_by_xpath(
             '//div[@title="Menu"]')
         ham.click()
         time.sleep(4)
+        # searches for the logout span and clicks it
         ham.find_element_by_xpath(
             '//div[@class="_2hHc6 T6CTG"]/ul[@class="_3z3lc"]/li[@class="_3cfBY _2yhpw _3BqnP"]/div[@title="Log out"]').click()
 
     # method that gets any new command/msg sent by the
     # user and stores it in a text file
     def learn(self, new_word):
+        # get the global varibale
         global learn_count
         found = False
+        # open the new text file to store the new words
         with open('new_words.txt', 'r+') as f:
             for line in f:
+                # check if the word already exists in the file
+                # to avoid duplication
                 if str(new_word) in line:
-                    print(line)
                     found = True
                     break
-
+            # store if not there already
+            # stores with the current date and time + the target name : msg
             if not found:
                 f.write(f'{self.target}-{str(time.ctime())}: {new_word}\n')
                 learn_count += 1
@@ -89,6 +98,7 @@ class Whatsapp:
 
     # method that returns the last msg
     # sent by the recipient
+    # ! problem : Cant handle multiple msgs
     def get_msg(self):
         no_div = False
         # scrolls to the height of the page
@@ -115,35 +125,92 @@ class Whatsapp:
 
         try:
             # gets the list of msgs recieved
+            # ! SINGLE MESSAGE
             msgs = [msg.text for msg in self.layer.find_elements_by_xpath(
                 "//div[@class='_1zGQT _2ugFP message-in tail']//span[@class='selectable-text invisible-space copyable-text']")]
-            print(msgs)
+            # print(msgs)
             # returns the last index of the list result
             return msgs[-1].lower()
+
+            # gets the list of emojis recieved
+            # ! EMOJI MESSAGES
+            emojis = [emoji for emoji in self.layer.find_elements_by_xpath(
+                "//div[@class='_1zGQT _2ugFP message-in']//img[@class='_298rb _2FANH selectable-text invisible-space copyable-text']")]
+            print(emoji.demojize(emojis))
+
         except Exception:
             no_div = True
 
         if no_div:
             try:
                 # gets the list of msgs recieved
+                # ! MUTIPLE MESSAGES
                 msgs = [msg.text for msg in self.layer.find_elements_by_xpath(
                     "//div[@class='_1zGQT _2ugFP message-in']//span[@class='selectable-text invisible-space copyable-text']")]
-                print(msgs)
+                # print(msgs)
                 # returns the last index of the list result
                 return msgs[-1]
+
+                # gets the list of emojis recieved
+                # ! EMOJI MESSAGES
+                emojis = [emoji for emoji in self.layer.find_elements_by_xpath(
+                    "//div[@class='_1zGQT _2ugFP message-in']//img[@class='_298rb _2FANH selectable-text invisible-space copyable-text']")]
+                print(emoji.demojize(emojis))
+
             except Exception as e:
                 print(e)
         else:
             pass
 
+    # searches through the json file for a reply suitable for the sent msg
+    # returns and empty list if no replies are avaialable
+    # TODO : Test accuracy and how long/Fast it can hanle the user input
+    @staticmethod
+    def reply_msg(msg):
+        # open and load the intents json file that has the chat patterns and replies
+        with open('intents.json') as f:
+            data = json.load(f)
+        # using the data in the json file open
+        # check in each pattern if the entered msg exists
+        # if it does, then choose a random reply and send it
+        for i in data["intents"]:
+            for pattern in i["patterns"]:
+                # using regex to match the exact sentence
+                if re.findall(r'^({0}\s*)$'.format(msg), pattern, re.M):
+                    # choose a random msg
+                    reply = random.choice(i["responses"])
+                    print(reply)
+                    # return the reply msg
+                    return reply
+
+    # this method finds the input box and send button
+    # gets the msg recieved from the param and sends it
+    # TODO : Crashes ( for a short period of seconds ) if 2 messages are sent at once.
+    def send_msg(self, msg):
+        # find the text box
+        input_box = self.driver.find_element_by_xpath(
+            '//div[@class="_3u328 copyable-text selectable-text"]')
+        # focus on it by clicking
+        input_box.click()
+        # send the msg value as the reply
+        input_box.send_keys(msg)
+        # find the send button and click it
+        self.driver.find_element_by_xpath(
+            '//span[@data-icon="send"]').click()
+        # once the msg is sent . clear the text box
+        # to avoid old msgs being sent
+        input_box.clear()
+
     # method that gets the msgs and replied to
     # them as programmed
+    # TODO : Testing required
     def chat(self):
+        reply = ''
         IsNew = False
         # run the class continuosly for each 10 seconds
         threading.Timer(10, self.chat).start()
         try:
-            last_msg = self.get_msg()
+            last_msg = self.get_msg().lower()
             print(f"last msg by {self.target}:  "+last_msg)
             # gets all the divs that contains the msgs (in and out)
             last_reply = [msg.get_attribute('data-pre-plain-text') for msg in self.layer.find_elements_by_xpath(
@@ -162,48 +229,28 @@ class Whatsapp:
             # if the last msg was sent or recieved
             # will run only if the last msg is recieved
             if IsNew:
-                # using the data in the json file open
-                # check in each pattern if the entered msg exists
-                # if it does, then choose a random reply and send it
-                for i in self.data["intents"]:
-                    for pattern in i["patterns"]:
-                        # using regex to match the exact sentence 
-                        if re.findall(r'^({}\s*)$'.format(last_msg), pattern, re.M):
-                            reply = random.choice(i["responses"])
-                            print(reply)
-                        else:
-                            # from the list of words that come in handy when the bot cant process 
-                            # or find what the recipient said
-                            reply = random.choice(no_reply)
-                            # add the new word to the txt file
-                            self.learn(last_msg)
-
-                # find the text box
-                input_box = self.driver.find_element_by_xpath(
-                    '//div[@class="_3u328 copyable-text selectable-text"]')
-                # focus on it by clicking
-                input_box.click()
-                # send the msg value as the reply
-                input_box.send_keys(reply)
-                # find the send button and click it
-                self.driver.find_element_by_xpath(
-                    '//span[@data-icon="send"]').click()
-                # once the msg is sent . clear the text box
-                # to avoid old msgs being sent
-                input_box.clear()
+                # checks if there is a reply available
+                # if yes returns the list containing the one random reply
+                reply = self.reply_msg(last_msg)
+                if reply:
+                    # send the msg using the send msg method
+                    self.send_msg(reply)
+                else:
+                    # no_reply is the list of words that come in handy when the bot cant process
+                    # or find what the recipient said, chose a random reply and send it
+                    self.send_msg(random.choice(no_reply))
+                    # add the new word to the txt file
+                    self.learn(last_msg)
             else:
                 print("You replied last")
 
         except Exception as e:
-            print(f"Error : {e}")
+            print(f"Error : {e}\n\n")
+            print(f"New messages : {learn_count}")
             self.logout()
-
-
 
 
 # create instance of class
 if __name__ == '__main__':
     bot = Whatsapp()
     bot.chat()
-
-print(f"New messages : {learn_count}")
